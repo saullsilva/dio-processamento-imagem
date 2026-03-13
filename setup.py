5d92eb44b8f8703bc1188445ef3750f500a6cb3d
saullsilva <saulassuncao8@gmail.com>
2026-03-13 01:03:21 -0300

    ajustando setup.py

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="dio-desafio-pacote-processamento-imagem",
+    name="dio-processamento-imagem",
     version="0.0.10",
-    author="Charles",
+    author="Saul Assunção",
     description="Processamento imagem usando Skimage",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/charlesfsouza/dio-desafio-pacote-processamento-imagem",
+    url="https://github.com/saullsilva/dio-processamento-imagem",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.5'
